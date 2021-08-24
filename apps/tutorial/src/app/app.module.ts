868cc25e09151e7d1b04e3358d320c631e883878
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2021-08-24 17:33:46 +0900

    module

--- apps/tutorial/src/app/app.module.ts
+++ apps/tutorial/src/app/app.module.ts
@@ -1,13 +1,8 @@
 import { Module } from '@nestjs/common';
-import { StudentController } from './student/student.controller';
-import { StudentTeacherController } from './teacher/student.controller';
-import { TeacherController } from './teacher/teacher.controller';
-import { StudentService } from './student/student.service';
-import { TeacherService } from './teacher/teacher.service';
+import { StudentModule } from './student/student.module';
+import { TeacherModule } from './teacher/teacher.module';
 
 @Module({
-  imports: [],
-  controllers: [StudentController, StudentTeacherController, TeacherController],
-  providers: [StudentService, TeacherService],
+  imports: [StudentModule, TeacherModule],
 })
 export class AppModule {}
