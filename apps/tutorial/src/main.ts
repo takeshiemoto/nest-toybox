f13bf3ba3d513245029e23fef86d9a684dee7aab
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2021-08-24 21:51:15 +0900

    dev

--- apps/tutorial/src/main.ts
+++ apps/tutorial/src/main.ts
@@ -11,17 +11,14 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
 
-  const config = new DocumentBuilder()
-    .setTitle('tutorial')
-    .setDescription('tutorial API description')
-    .setVersion('1.0')
-    .build();
+  const config = new DocumentBuilder().setTitle('Tutorial').build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
   await app.listen(port, () => {
     Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
