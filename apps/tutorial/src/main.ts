9c6ee8d9a28ff211e31cf76e6482695f42d2d257
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2021-08-24 18:16:46 +0900

    swagger

--- apps/tutorial/src/main.ts
+++ apps/tutorial/src/main.ts
@@ -13,10 +13,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('tutorial')
+    .setDescription('tutorial API description')
     .setVersion('1.0')
-    .addTag('cats')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
