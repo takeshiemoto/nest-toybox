76b256981dc664ad1aa292c4576c4ff75833433e
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2021-08-26 15:03:55 +0900

    update db

--- apps/tutorial/src/app/auth/auth.module.ts
+++ apps/tutorial/src/app/auth/auth.module.ts
@@ -14,7 +14,7 @@ import { JwtStrategy } from './jwt.strategy';
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '3m' },
+      signOptions: { expiresIn: '1h' },
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
--- apps/tutorial/src/db.ts
+++ apps/tutorial/src/db.ts
@@ -1,34 +1,19 @@
 export const students = [
   {
-    id: '1c25025e-b656-11eb-8529-0242ac130003',
+    id: '0e395870-9143-419d-b2ce-27bef8c339ec',
     name: 'Anthony Cole',
     teacher: '9c9324e8-b656-11eb-8529-0242ac130003',
   },
   {
     id: '1c2504f2-b656-11eb-8529-0242ac130003',
     name: 'Micheal Bryant',
-    teacher: '9c9324e8-b656-11eb-8529-0242ac130003',
-  },
-  {
-    id: '1c2505d8-b656-11eb-8529-0242ac130003',
-    name: 'Kobe Jordan',
-    teacher: '9c9324e8-b656-11eb-8529-0242ac130003',
-  },
-  {
-    id: '1c250696-b656-11eb-8529-0242ac130003',
-    name: 'Laith Harb',
-    teacher: '9c932736-b656-11eb-8529-0242ac130003',
-  },
-  {
-    id: '1c250754-b656-11eb-8529-0242ac130003',
-    name: 'Kendrick Jenner',
-    teacher: '9c932736-b656-11eb-8529-0242ac130003',
+    teacher: 'f84fc12b-3cf5-47e0-9b03-4719eceff413',
   },
 ];
 
 export const teachers = [
   {
-    id: '9c9324e8-b656-11eb-8529-0242ac130003',
+    id: 'f84fc12b-3cf5-47e0-9b03-4719eceff413',
     name: 'Ms Jackson',
   },
   {
@@ -40,8 +25,8 @@ export const teachers = [
 export const users = [
   {
     id: '98a41b85-e88c-7ad4-45a6-8fe575a7c77c',
-    username: 'john',
-    password: 'changeme',
+    username: 'tete',
+    password: 'tete',
   },
   {
     id: '3944c584-d14c-e4bf-c03a-d9ebb8abce2d',
